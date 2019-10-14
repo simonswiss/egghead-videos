4f448be306dcb992c37c5c6170ff5a11fbc4d790
Simon Vrachliotis <simon@simonswiss.com>
2019-10-15 09:12:37 +1100

    emotion-vanilla-js

--- lessons/emotion/emotion-vanilla-js/src/index.js
+++ lessons/emotion/emotion-vanilla-js/src/index.js
@@ -1,9 +1,24 @@
+import { css } from "emotion";
+
+const headingStyles = css`
+  font-family: sans-serif;
+  background-color: rebeccapurple;
+  color: white;
+  padding: 1rem 1.5rem;
+  border-radius: 2px;
+  transition: color 0.2s, background-color 0.2s;
+
+  :hover {
+    color: rebeccapurple;
+    background-color: #f2f2f2;
+  }
+`;
+
 // Create a heading (h1 tag)
 const heading = document.createElement("h1");
 heading.innerText = "Using emotion with vanilla JavaScript!";
+heading.classList.add(headingStyles);
 
-// Get the div where the app is injected
+// Append said heading to our app
 const app = document.getElementById("app");
-
-// Appending the heading to the app
 app.appendChild(heading);
